60dfe5e34263e6780b5760fc2bf91c424b81832c
Bochkovskyi <unundertale1@ukr.net>
2023-09-13 21:08:15 +0300

    add express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
