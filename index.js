322d6dbc1227b6770f7a36586040c22b3c1bbedd
Bochkovskyi <unundertale1@ukr.net>
2023-09-14 20:46:03 +0300

    add excel data upload to db

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const multer = require("multer");
+const readXlsxFile = require("read-excel-file/node");
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
+const mysqlPool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+  })
+  .promise();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, process.cwd() + "/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
 });
 
-connection.connect((err) => {
-  if (err) {
-    return console.error(`MySQL connection error: ${err.message}`);
-  } else {
-    return console.log("Successfully connected to MySQL");
-  }
-});
-
-const query = `SELECT * FROM test`;
-
-connection.query(query, (err, results) => {
-  console.log(err);
-  console.log(results);
-});
+const uploadFile = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("home", { message: "Hello, world!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.post("/load-excel", uploadFile.single("load-excel"), async (req, res) => {
+  await loadDataToDB(process.cwd() + "/uploads/" + req.file.filename);
+  res.redirect("/");
 });
 
-connection.end((err) => {
-  if (err) {
-    return console.error(`MySQL connection error: ${err.message}`);
-  } else {
-    return console.log("MySQL connection successfully closed!");
+function loadDataToDB(excelFile) {
+  readXlsxFile(excelFile).then(async (rows) => {
+    // Remove Header ROW
+    rows.shift();
+
+    await mysqlPool.query(
+      "INSERT INTO test (name, description) VALUES ?",
+      [rows],
+      (error, results) => {
+        console.log(error || results);
+      }
+    );
+  });
+}
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+
+  if (!err.errorMessage) {
+    err.errorMessage = "Oh no, something went wrong!";
   }
+
+  console.error(err);
+  res.status(statusCode).send(err.errorMessage);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
