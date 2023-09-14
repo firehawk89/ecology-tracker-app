f311267d624f3285090f5953cd9aadea428bf2b0
Bochkovskyi <unundertale1@ukr.net>
2023-09-14 21:09:21 +0300

    move db and multer config to config folder

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const mysql = require("mysql2");
-const multer = require("multer");
+const mysqlPool = require("./config/db");
+const uploadFile = require("./config/multer");
 const readXlsxFile = require("read-excel-file/node");
 
 const app = express();
@@ -12,26 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-const mysqlPool = mysql
-  .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    database: process.env.MYSQL_DATABASE,
-    password: process.env.MYSQL_PASSWORD,
-  })
-  .promise();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, process.cwd() + "/uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
-  },
-});
-
-const uploadFile = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("home", { message: "Hello, world!" });
