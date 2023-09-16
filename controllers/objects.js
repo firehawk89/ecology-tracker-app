0fa9f62b64bda1313148759b8adee97ab98aa5d2
Bochkovskyi <unundertale1@ukr.net>
2023-09-16 20:35:25 +0300

    add excel load controller

--- controllers/objects.js
+++ controllers/objects.js
@@ -1,3 +1,25 @@
+const mysqlPool = require("../config/db");
+const readXlsxFile = require("read-excel-file/node");
+
 module.exports.index = (req, res) => {
   res.render("objects/index");
+};
+
+module.exports.loadExcelData = (req, res) => {
+  const excelFile = process.cwd() + "/uploads/" + req.file.filename;
+
+  readXlsxFile(excelFile).then((rows) => {
+    // Remove Header ROW
+    rows.shift();
+
+    mysqlPool.query(
+      "INSERT INTO test (name, description) VALUES ?",
+      [rows],
+      (error, results) => {
+        console.log(error || results);
+      }
+    );
+  });
+
+  res.redirect("/objects");
 };
