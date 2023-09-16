67f6913cd2241024c287e49f82717cb42c1f3ac7
Bochkovskyi <unundertale1@ukr.net>
2023-09-16 20:36:06 +0300

    add objects post route

--- routes/objects.js
+++ routes/objects.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const objects = require("../controllers/objects");
+const uploadFile = require("../config/multer");
 
-router.route("/").get(objects.index);
+router
+  .route("/")
+  .get(objects.index)
+  .post(uploadFile.single("load-excel"), objects.loadExcelData);
 
 module.exports = router;
