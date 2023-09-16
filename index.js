a456c394dbbae9d94eec4b3f0063c2c28d27ef66
Bochkovskyi <unundertale1@ukr.net>
2023-09-16 21:11:25 +0300

    init pollutants routes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 const objectsRoutes = require("./routes/objects");
+const pollutantsRoutes = require("./routes/pollutants");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,6 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use("/objects", objectsRoutes);
+app.use("/pollutants", pollutantsRoutes);
 
 app.get("/", (req, res) => {
   res.render("home", { message: "Hello, world!" });
