310405c1fde34893aa3b3a1bf743896b6d482202
Bochkovskyi <unundertale1@ukr.net>
2023-09-22 17:34:56 +0300

    add method override lib

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const app = express();
 const PORT = 3000;
@@ -8,11 +9,12 @@ const objectsRoutes = require("./routes/objects");
 const pollutantsRoutes = require("./routes/pollutants");
 const AppError = require("./utils/AppError");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use("/objects", objectsRoutes);
 app.use("/pollutants", pollutantsRoutes);
@@ -27,6 +29,8 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Oh no, something went wrong!" } = err;
+
+  console.log(err);
 
   res.status(status).render("error", { message });
 });
