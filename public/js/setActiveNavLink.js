6efadf859948a270f11012cbeabc69505522af58
Bochkovskyi <unundertale1@ukr.net>
2023-09-24 19:23:59 +0300

    change active navlink detetction logic

--- public/js/setActiveNavLink.js
+++ public/js/setActiveNavLink.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".nav-link").forEach((navLink) => {
-  if (navLink.pathname === window.location.pathname) {
+  if (window.location.pathname.indexOf(navLink.pathname) > -1) {
     navLink.classList.add("active");
   } else {
     navLink.classList.remove("active");
