25df4ae6f028d0a0eb0e07730af30150d8d081bd
Bochkovskyi <unundertale1@ukr.net>
2023-09-24 21:42:25 +0300

    add pollutions index route

---
+++ routes/pollutions.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const pollutionControllers = require("../controllers/pollutions");
+const uploadFile = require("../config/multer");
+
+router.route("/").get(pollutionControllers.index);
+//   .post(pollutionControllers.addNewPollution);
+
+// router.route("/new").get(pollutionControllers.renderNewPollutionForm);
+
+// router
+//   .route("/load-excel")
+//   .post(uploadFile.single("load-excel"), pollutionControllers.loadFromExcel);
+
+// router
+//   .route("/:pollutantId")
+//   .put(pollutionControllers.updatePollutant)
+//   .delete(pollutionControllers.deletePollutant);
+
+// router
+//   .route("/:pollutantId/edit")
+//   .get(pollutionControllers.renderEditPollutantForm);
+
+module.exports = router;
