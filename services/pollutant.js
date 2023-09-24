da23d653433b7d964b3c9f23c004547e8173d2b0
Bochkovskyi <unundertale1@ukr.net>
2023-09-24 20:54:41 +0300

    add function for inserting one pollutant

--- services/pollutant.js
+++ services/pollutant.js
@@ -5,6 +5,19 @@ module.exports.getAll = async () => {
   return rows;
 };
 
+module.exports.insertOne = async (pollutant) => {
+  await mysqlPool.query(
+    "INSERT INTO pollutant (pollutant_name, min_mass_consumption, max_mass_consumption, gdk, danger_class) VALUES (?, ?, ?, ?, ?)",
+    [
+      pollutant.name,
+      pollutant.minMassCons,
+      pollutant.maxMassCons,
+      pollutant.gdk,
+      pollutant.dangerClass,
+    ]
+  );
+};
+
 module.exports.insertMany = async (rows) => {
   await mysqlPool.query(
     "INSERT INTO pollutant (pollutant_name, gdk, min_mass_consumption, max_mass_consumption, danger_class) VALUES ?",
