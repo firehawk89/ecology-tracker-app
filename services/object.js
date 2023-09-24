b6408ef4ae5437ecf4f479c11b1395a3e97f3630
Bochkovskyi <unundertale1@ukr.net>
2023-09-24 19:22:55 +0300

    add functions for getting and updating object by id

--- services/object.js
+++ services/object.js
@@ -5,6 +5,14 @@ module.exports.getAll = async () => {
   return rows;
 };
 
+module.exports.getById = async (id) => {
+  const [row] = await mysqlPool.query(
+    "SELECT * FROM object WHERE object_id = ?",
+    [id]
+  );
+  return row[0];
+};
+
 module.exports.insertOne = async (object) => {
   await mysqlPool.query(
     "INSERT INTO object (object_name, activity, ownership_form, address) VALUES (?, ?, ?, ?)",
@@ -17,6 +25,14 @@ module.exports.insertMany = async (rows) => {
     "INSERT INTO object (object_name, activity, ownership_form, address) VALUES ?",
     [rows]
   );
+};
+
+module.exports.updateOneById = async (id, object) => {
+  const result = await mysqlPool.query(
+    "UPDATE object SET object_name = ?, activity = ?, ownership_form = ?, address = ? WHERE object_id = ?",
+    [object.name, object.activity, object.ownershipForm, object.address, id]
+  );
+  return result[0].affectedRows;
 };
 
 module.exports.deleteOneById = async (id) => {
