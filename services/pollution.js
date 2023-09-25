6c6a631b8d1f0c24076b3ae1164af12564e9248c
Bochkovskyi <unundertale1@ukr.net>
2023-09-25 16:39:58 +0300

    update services

--- services/pollution.js
+++ services/pollution.js
@@ -1,8 +1,9 @@
 const mysqlPool = require("../config/db");
+const pollutantService = require("./pollutant");
 
 module.exports.getAll = async () => {
   const [rows] = await mysqlPool.query(
-    `SELECT pollution.pollution_id, object.object_name, pollutant.pollutant_name, pollution_value, pollution_year 
+    `SELECT pollution.pollution_id, object.object_name, pollutant.pollutant_name,pollution. pollution_value, pollution.pollution_year 
 	FROM pollution 
 	INNER JOIN object 
 	ON pollution.object_id = object.object_id 
@@ -15,24 +16,31 @@ module.exports.getAll = async () => {
 
 module.exports.getById = async (id) => {
   const [row] = await mysqlPool.query(
-    "SELECT * FROM pollution WHERE pollution_id = ?;",
+    `SELECT pollution.pollution_id, object.object_name, pollutant.pollutant_name, pollution.pollution_value, pollution.pollution_year 
+	FROM pollution 
+	INNER JOIN object 
+	ON pollution.object_id = object.object_id 
+	INNER JOIN pollutant 
+	ON pollution.pollutant_id = pollutant.pollutant_id 
+	WHERE pollution_id = ?;`,
     [id]
   );
   return row[0];
 };
 
-module.exports.insertOne = async (pollution) => {
-  const [foundObject] = await mysqlPool.query(
+module.exports.getIdByName = async (name) => {
+  const [foundRow] = await mysqlPool.query(
     "SELECT object_id FROM object WHERE object_name = ?;",
-    [pollution.object]
+    [name]
   );
-  const objectId = foundObject[0].object_id;
+  const id = foundRow[0].object_id;
 
-  const [foundPollutant] = await mysqlPool.query(
-    "SELECT pollutant_id FROM pollutant WHERE pollutant_name = ?;",
-    [pollution.pollutant]
-  );
-  const pollutantId = foundPollutant[0].pollutant_id;
+  return id;
+};
+
+module.exports.insertOne = async (pollution) => {
+  const objectId = await this.getIdByName(pollution.object);
+  const pollutantId = await pollutantService.getIdByName(pollution.pollutant);
 
   await mysqlPool.query(
     "INSERT INTO pollution (object_id, pollutant_id, pollution_value, pollution_year) VALUES (?, ?, ?, ?);",
@@ -49,9 +57,12 @@ module.exports.insertMany = async (rows) => {
 };
 
 module.exports.updateOneById = async (id, pollution) => {
+  const objectId = await this.getIdByName(pollution.object);
+  const pollutantId = await pollutantService.getIdByName(pollution.pollutant);
+
   const result = await mysqlPool.query(
     "UPDATE pollution SET object_id = ?, pollutant_id = ?, pollution_value = ?, pollution_year = ? WHERE pollution_id = ?;",
-    [pollution.object, pollution.pollutant, pollution.value, pollution.year, id]
+    [objectId, pollutantId, pollution.value, pollution.year, id]
   );
   return result[0].affectedRows;
 };
