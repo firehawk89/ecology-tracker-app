55e27047e56da0a43ff5ac861d286879e2dab145
Bochkovskyi <unundertale1@ukr.net>
2023-12-19 20:04:07 +0200

    adjust getAll function

--- services/emergency.services.js
+++ services/emergency.services.js
@@ -1,6 +1,20 @@
 const mysqlPool = require("../config/db");
 
-module.exports.getAll = async (objectId, year) => {
-  const [rows] = await mysqlPool.query("CALL victims(?, ?);", [objectId, year]);
+module.exports.getAll = async (
+  objectId,
+  year,
+  child_age,
+  damage_area,
+  q,
+  dead_fish
+) => {
+  const [rows] = await mysqlPool.query("CALL victims(?, ?, ?, ?, ?, ?);", [
+    objectId,
+    year,
+    child_age,
+    damage_area,
+    q,
+    dead_fish,
+  ]);
   return rows[0];
 };
